5c9980670755a363c1ac9951c4a164c5c7b74e36
petervol27 <petervol35@gmail.com>
2024-10-01 22:12:08 +0300

    what

--- general.js
+++ general.js
@@ -1,3 +1,3 @@
 const prodHost = 'https://my-shop-django.onrender.com';
 const devHost = 'http://127.0.0.1:8000';
-const imageUrl = 'https://petervol27.github.io/static_django/static';
+const imageUrl = 'https://petervol27.github.io/shop_static/static';
