c9fb19e297aea41f192756ae82462adbb5374867
Pietro <petervol35@gmail.com>
2024-10-06 20:34:11 +0300

    test

--- general.js
+++ general.js
@@ -1,3 +1,10 @@
 const prodHost = 'https://my-shop-django.onrender.com';
+
 const devHost = 'http://127.0.0.1:8000';
+
 const imageUrl = 'https://petervol27.github.io/shop_static/static';
+
+const checkLogin = async () => {
+  const response = await axios.get(`${prodHost}/users/check_login`);
+  console.log(response.data);
+};
