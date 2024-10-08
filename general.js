5e33f36311b179c25f696962f17490551a6f854d
petervol27 <petervol35@gmail.com>
2024-10-08 19:56:23 +0300

    suka nahuy

--- general.js
+++ general.js
@@ -2,8 +2,6 @@ const prodHost = 'https://products-shop-17p6.onrender.com';
 
 const devHost = 'http://127.0.0.1:8000';
 
-const imageUrl = 'https://petervol27.github.io/shop_static/static';
-
 // const checkLogin = async () => {
 //   try {
 //     const response = await axios.get(`${devHost}/users/check_login`, {
